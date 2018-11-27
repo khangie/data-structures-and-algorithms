2f97d9a8511f32fa8ce63b066e1907988a486c12
Khang Nguyen <khangie@hotmail.com>
2018-11-27 12:59:54 -0800

    bfs updates:

--- src/com/graph/GraphMain.java
+++ src/com/graph/GraphMain.java
@@ -43,15 +43,6 @@ public class GraphMain {
 	 *    a) Each city can be represented as vertices
 	 *    b) The road used to connect them are edges
 	 * 
-	 * Breadth-first search (BFS)
-	 * 1) Running time complexity is O(V+E)
-	 * 2) Memory complexity is not good since we have to store lots of references
-	 * 3) This is why Depth-first search (DFS) is usually preferred
-	 * 4) Constructs a shortest path
-	 * 5) Implementation
-	 *    a) We have an empty queue at the beginning and we keep checking whether we have visited the given node or not
-	 *    b) Keep iterating until queue is not empty
-	 * 
 	 * @param args
 	 */
 	
--- src/com/graph/bfs/BreadthFirstSearchMain.java
+++ src/com/graph/bfs/BreadthFirstSearchMain.java
@@ -2,6 +2,18 @@ package com.graph.bfs;
 
 public class BreadthFirstSearchMain {
 	
+	/**
+	 * Summary
+	 * 1) Running time complexity is O(V+E)
+	 * 2) Memory complexity is not good since we have to store lots of references
+	 * 3) This is why Depth-first search (DFS) is usually preferred
+	 * 4) Constructs a shortest path
+	 * 5) Implementation
+	 *    a) We have an empty queue at the beginning and we keep checking whether we have visited the given node or not
+	 *    b) Keep iterating until queue is not empty
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
 		
 		BreadthFirstSearch breadFirstSearch = new BreadthFirstSearch();
